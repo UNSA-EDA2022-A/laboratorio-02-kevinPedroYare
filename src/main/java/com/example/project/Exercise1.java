4c61fccb8cdb43de591672fff76ee8a682f4c4ec
kevinPedroYare <83080715+kevinPedroYare@users.noreply.github.com>
2022-05-13 12:16:18 -0500

    Update Exercise1.java

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -15,7 +15,30 @@ public class Exercise1 {
 	}
 
 	public boolean esCuadradoPerfecto(int numero) {
-
-		return false;
+		//iniciamos un auxiliar que empieze en 1 que sera nuestro numero de para verificar
+		//si es raiz de numero
+		int aux = 1;
+		//verificamos si con el primer auxiliar es raiz de numero
+		if(aux*aux == numero){
+			//si es raiz de numero entonces retornamos true
+			return true;
+		}else{
+			//caso contrario llamara al otro metodo con la condicion extra el numero y el aux aumentado en 1
+			return esCuadradoPerfecto(numero,aux+1);
+		}
+	}
+	//se añadio otro metodo similar al anterior, pero con una condicion extra
+	public boolean esCuadradoPerfecto(int numero, int aux) {
+		//silimar al anterior verificamos si con el auxiliar es raiz de numero
+		if(aux*aux == numero){
+			return true;
+		}
+		//aca vemos si ya el aux al cuadrado es mayor al numero base o es 0 retornara false
+		else if(aux*aux > numero || numero == 0){
+			return false;
+		}else{
+			//retornara el numero y el auxiliar aumentado en 1 si no se cumplio lo demas haciendo recursividad
+			return esCuadradoPerfecto(numero,aux+1);
+		}
 	}
 }
